8218ee9c424821377b9b9ffc5ebe4860b2481036
Szymon Sauer <szymonsauer1@gmail.com>
2021-08-16 12:16:30 +0200

    todo list

---
+++ script.js
@@ -0,0 +1,119 @@
+let todoInput; //place for activity
+let errorInfo; //information about errors
+let addBtn; //button which adds new elements
+let ulList; //list of tasks
+let neweTodo; //new task
+let popup; //popup
+let popupInfo; //text in popup
+let todoToEdit; //edited task
+let popupInput; //input in popup
+let popupAddBtn; //add button in popup
+let popupCloseBtn; //cancel button in popup
+
+const main = () => {
+	prepareDOMElements();
+	prepareDOMEvents();
+};
+
+const prepareDOMElements = () => {
+	todoInput = document.querySelector('.todo-input');
+	errorInfo = document.querySelector('.error-info');
+	addBtn = document.querySelector('.btn-add');
+	ulList = document.querySelector('.todolist ul');
+
+	popup = document.querySelector('.popup');
+	popupInfo = document.querySelector('.popup-info');
+	popupInput = document.querySelector('.popup-input');
+	popupAddBtn = document.querySelector('.accept');
+	popupCloseBtn = document.querySelector('.cancel');
+};
+
+const prepareDOMEvents = () => {
+	addBtn.addEventListener('click', addNewTodo);
+	ulList.addEventListener('click', checkClick);
+	popupCloseBtn.addEventListener('click', closePopup);
+	popupAddBtn.addEventListener('click', changeTodoText);
+	todoInput.addEventListener('keyup', enterKeyCheck);
+};
+
+const addNewTodo = () => {
+	if (todoInput.value !== '') {
+		newTodo = document.createElement('li');
+		newTodo.textContent = todoInput.value;
+		ulList.append(newTodo);
+		errorInfo.innerText = '';
+		createToolsArea();
+	} else {
+		errorInfo.textContent = 'Add new task...';
+	}
+};
+
+const createToolsArea = () => {
+	const toolsPanel = document.createElement('div');
+	toolsPanel.classList.add('tools');
+	newTodo.append(toolsPanel);
+
+	const completeBtn = document.createElement('button');
+	completeBtn.classList.add('complete');
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+	const editBtn = document.createElement('button');
+	editBtn.classList.add('edit');
+	editBtn.textContent = 'EDIT';
+
+	const deleteBtn = document.createElement('button');
+	deleteBtn.classList.add('delete');
+	deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+	toolsPanel.append(completeBtn, editBtn, deleteBtn);
+};
+
+const checkClick = (e) => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed');
+		e.target.classList.toggle('completed');
+	} else if (e.target.matches('.edit')) {
+		editTodo(e);
+	} else if (e.target.matches('.delete')) {
+		deleteTodo(e);
+	}
+};
+
+const editTodo = (e) => {
+	todoToEdit = e.target.closest('li');
+	popupInput.value = todoToEdit.firstChild.textContent;
+	console.log(todoToEdit.firstChild);
+	popup.style.display = 'flex';
+};
+
+const changeTodoText = () => {
+	if (popupInput.value !== '') {
+		todoToEdit.firstChild.textContent = popupInput.value;
+		popup.style.display = 'none';
+		popupInfo.textContent = '';
+	} else {
+		popupInfo.textContent = 'You have to write something...';
+	}
+};
+
+const deleteTodo = (e) => {
+	e.target.closest('li').remove();
+	const allTodos = ulList.querySelectorAll('li');
+
+	if (allTodos.length === 0) {
+		errorInfo.innerText = 'No tasks on the list...';
+	}
+};
+
+const enterKeyCheck = (e) => {
+	if (e.key === 'Enter') {
+		addNewTodo();
+	}
+};
+
+const closePopup = () => {
+	popup.style.display = 'none';
+	popupInfo.innerText = '';
+};
+
+document.addEventListener('DOMContentLoaded', main);
